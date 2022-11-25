c91f6914a0e247f518755ca9331705f47e77ecd4
K.S.Jeong <jks2024@gmail.com>
2022-11-25 14:30:10 +0900

    register page add

--- shop/src/main/java/com/shop/controller/MemberController.java
+++ shop/src/main/java/com/shop/controller/MemberController.java
@@ -1,11 +1,14 @@
 package com.shop.controller;
 
 import com.shop.dto.MemberFromDto;
+import com.shop.entity.Member;
 import com.shop.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/members")
@@ -13,10 +16,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 public class MemberController {
     private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
     @GetMapping("/new")
     public String memberForm(Model model) {
         model.addAttribute("memberFormDto", new MemberFromDto());
         return "member/memberForm";
+    }
+    @PostMapping("/new")
+    public String memberForm(MemberFromDto memberFromDto) {
+        Member member = Member.createMember(memberFromDto, passwordEncoder);
+        memberService.saveMember(member);
+        return "redirect:/";
     }
 
 }
